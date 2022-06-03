29136dfefea0ab5d5bebb971b2f48579bffdcdf2
Tian Khamez <tkhamez@gmail.com>
2022-06-03 21:23:01 +0200

    Adjust cron jobs, update to v1.32.0.

--- files/run-jobs-8.sh
+++ files/run-jobs-8.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 118m, 3m, 80m, 0m, 1?m = 201m
-/var/app/current/backend/bin/console check-tokens --log --hide-details
+/var/app/current/backend/bin/console check-tokens --characters active --log --hide-details
 /var/app/current/backend/bin/console send-invalid-token-mail --log
 /var/app/current/backend/bin/console update-member-tracking --log
 /var/app/current/backend/bin/console send-missing-character-mail --log
---
+++ files/weekly.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/var/app/current/backend/bin/console check-tokens --characters other --log --hide-details
