433817f41cd13b2065dd8c6cede6b25c327d7619
Tian Khamez <tkhamez@gmail.com>
2022-07-31 12:25:33 +0200

    Env vars: Replace sed with get-config and jq.

--- .platform/hooks/postdeploy/01_set_env.sh
+++ .platform/hooks/postdeploy/01_set_env.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# copy file and add "export " at the beginning
-sed -E -n 's/[^#]+/export &/ p' /opt/elasticbeanstalk/deployment/env > /opt/elasticbeanstalk/deployment/custom_env_var
-
-# add quotation marks around values
-sed 's/\(=[[:blank:]]*\)\(.*\)/\1"\2"/' -i /opt/elasticbeanstalk/deployment/custom_env_var
-
-chmod 644 /opt/elasticbeanstalk/deployment/custom_env_var
-rm -f /opt/elasticbeanstalk/deployment/*.bak
+/opt/elasticbeanstalk/bin/get-config environment | jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' > /opt/elasticbeanstalk/deployment/env-vars
